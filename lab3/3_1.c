3377440ed37e576d308762bd2e1b9f32a4f7735c
Misha_si <128136138+GLISTMISHA2@users.noreply.github.com>
2024-10-03 08:01:10 +0700

    Create 3_1.c

---
+++ lab3/3_1.c
@@ -0,0 +1,15 @@
+//№2
+#include <stdio.h>
+//чето не ясно че это 
+int main()
+{
+    int a;
+    int b;
+    printf("введите а");
+    scanf("%d",&a);
+    printf("введите b");
+    scanf("%d",&b);
+    for(int n=a;n%a!=0 || n%b!=0; n++){
+        printf("%d",n);
+    }
+}

dc2bc5311f382cd73a6324f8e6380d2c4e45dd61
Misha_si <128136138+GLISTMISHA2@users.noreply.github.com>
2024-10-30 23:25:41 +0700

    Create task on 5.c

---
+++ lab7/task on 5.c
@@ -0,0 +1,26 @@
+// вариант 2	"В исходном предложении удалить все символы пробела. Вывести преобразованный текст" 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    int length = strlen(text);
+    int count = 0;
+
+    for (int i = 0; i < length; i++) { // Вычисляем количество пробелов
+        if (text[i] == ' ') {
+            count++;
+        }
+    }
+    
+    int j = 0;
+    for (int i = 0; i < length; i++) { // Удаляем пробелы
+        if (text[i] != ' ') {
+            text[j++] = text[i];
+        }
+    }
+    text[j] = '\0'; // Добавляем завершающий нулевой символ
+
+    printf("Преобразованный текст: %s\n", text);
+    printf("Количество удаленных пробелов: %d\n", count);
+}
